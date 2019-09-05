a4fa41b665483d7536fb5fc1973a8e7973b3327f
Yeshi <tyeshi777@yahoo.com>
2019-09-05 12:47:38 -0700

    chain endpoint

--- basic_block_gp/blockchain.py
+++ basic_block_gp/blockchain.py
@@ -198,6 +198,8 @@ def new_transaction():
 def full_chain():
     response = {
         # TODO: Return the chain and its current length
+        "chain": blockchain.chain,
+        "length": len(blockchain.chain)
     }
     return jsonify(response), 200
 
